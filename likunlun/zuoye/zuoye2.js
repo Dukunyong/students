a11cef3aa7262b3380f61ddcc34149d6a7d8bcc5
john <me@youlkjhg.com>
2017-06-06 00:44:54 +0800

    提交作业

--- likunlun/zuoye/zuoye2.js
+++ likunlun/zuoye/zuoye2.js
@@ -10,6 +10,7 @@ BMW.prototype = {
 		this.createOutArc();
 		this.createWhiteCir();
 		this.createSector();
+		this.createText();
 	},
 	clearRect:function(){
 		this.ctx.clearRect(0,0,500,500);
@@ -38,15 +39,38 @@ BMW.prototype = {
 		this.ctx.beginPath()
 		var colors = [ "#069ffd", "#fff" ]; 
 		var startAngle = 0;
-		var arc = Math.PI / 2; 
-		this.ctx.translate(250,250);
-		for(var i=0;i<4;i++){
-			var angle = startAngle + i * arc;
-			this.ctx.fillStyle = colors[i%2];
-			this.ctx.beginPath();
+		var arc = Math.PI / 2;
+		var insideRadius = 150;
+		for(var i = 0; i < 4; i++) {       
+			var angle = startAngle + i * arc;        
+			this.ctx.fillStyle = colors[i%2];                
+			this.ctx.beginPath();  
 			this.ctx.lineWidth = 0;
-			
-		}
+			this.ctx.arc(250, 250, insideRadius, angle, angle + arc, false);        
+			this.ctx.arc(250, 250, 0, angle + arc, angle, true);          
+			this.ctx.fill();                
+			this.ctx.save();       
+	    }   
+		this.ctx.restore();      
+	},
+	createText:function(){
+		this.ctx.fillStyle = 'white'; 
+		this.ctx.font = "normal 70px 'Arial'";
+		this.ctx.textAlign = "center";
+		this.ctx.translate(250,250);
+
+		this.ctx.beginPath();
+		this.ctx.save();
+		this.ctx.rotate(-45*Math.PI/180);
+		this.ctx.fillText("B",-20,-165);
+		this.ctx.restore();
+
+		this.ctx.beginPath();
+		this.ctx.fillText("M",0,-165);
+
+		this.ctx.beginPath();
+		this.ctx.rotate(45*Math.PI/180);
+		this.ctx.fillText("W",20,-165);
+		this.ctx.restore();
 	}
-	
 }
