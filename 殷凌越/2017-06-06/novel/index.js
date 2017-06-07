9ea7263cd8035ae280c4f86a4b2031d18dc327ab
殷凌越 <yin_lingyue@163.com>
2017-06-07 17:43:35 +0800

    add

--- 殷凌越/2017-06-06/novel/index.js
+++ 殷凌越/2017-06-06/novel/index.js
@@ -9,48 +9,45 @@ $.extend(Novel.prototype, {
 	},
 
 	bindEvents: function() {
-		var handleFontAddBtn = $(".handleFontAddBtn"),
-			handleFontMinusBtn = $(".handleFontMinusBtn"),
-			handleColorChange = $(".handleColorChange"),
-			submitBtn = $(".js-submit"),
-			clearBtn = $(".js-clear");
-
-		submitBtn.on("click", $.proxy(this.handleSubmitClick, this));
-		clearBtn.on("click", $.proxy(this.handleClearClick, this))
+		var handleFontAddBtn = $(".js-handleFontAddBtn"),
+			handleFontMinusBtn = $(".js-handleFontMinusBtn"),
+			handleColorChangeBtn = $(".js-handleColorChangeBtn");	
 		handleFontAddBtn.on("click", $.proxy(this.handleFontAddClick, this)); 
 		handleFontMinusBtn.on("click", $.proxy(this.handleFontMinusClick, this));
-		handleColorChange.on("click","li", this.handleColorClick);
+		handleColorChangeBtn.on("click", $.proxy(this.handleColorChangeClick, this));
 	},
 
 	handleFontAddClick: function() { //字体变大
-		var size = parseInt($(".pageCount").css("fontSize"));
+		var size = parseInt($(".js-pageCount").css("fontSize"));
 		size+= 2;
 		//字体超过30时 不再增加 
 		if (size>=30) {
 			size=30;		
 		}
-		$(".pageCount").css({fontSize:size});
-		$(".handleFontRestoreBtn span").html(size);
+		$(".js-pageCount").css({fontSize:size});
+		$(".js-handleFontRestoreBtn span").html(size);
+		this.handleSubmitClick();
+
 	},
 
 	handleFontMinusClick: function() { //字体变小
-		var size = parseInt($(".pageCount").css("fontSize"));
+		var size = parseInt($(".js-pageCount").css("fontSize"));
 		size-= 2; 
 		//字体小于12时 不再减少 
 		if (size<=12) {
 			size=12;		
 		}
-		$(".pageCount").css({fontSize:size});
-		$(".handleFontRestoreBtn span").html(size);
+		$(".js-pageCount").css({fontSize:size});
+		$(".js-handleFontRestoreBtn span").html(size);
+		this.handleSubmitClick();
 	},
 
 
-	handleColorClick: function() { //切换背景色
-		var color= $(this).attr("data-color");
-		$(".pageCount").css("background",color)
+	handleColorChangeClick: function(e) { //切换背景色	
+		var color= $(e.target).attr("data-color");
+		$(".js-pageCount").css("background",color)
+		this.handleSubmitClick();
 	},
-
-
 
 
 	restoreUserInfo: function() {//恢复上一次用户信息
@@ -59,36 +56,29 @@ $.extend(Novel.prototype, {
 				var userInfo = {};
 				if (window.localStorage.userInfo) {
 			  		var userInfo = JSON.parse(window.localStorage.userInfo);
-			  		$(".pageCount").css("fontSize",window.userInfo.userSize);
-  					$(".pageCount").css("background",window.userInfo.userColors);
+			  		$(".js-pageCount").css("fontSize", userInfo.userSize);
+  					$(".js-pageCount").css("background", userInfo.userColor);
 			  	}
 			}
 		//catch必须传参
 		}catch(e){}
 	},
 
-	handleSubmitClick: function() {//提交
+	handleSubmitClick: function(size, color) {//提交
+
+
 		var userInfo = {
-			userSize: $(".pageCount").css("fontSize"),
-			userColor: $(".pageCount").css("background")
+			userSize: $(".js-pageCount").css("fontSize"),
+			userColor: $(".js-pageCount").css("backgroundColor")
 		};
+
 		
 		userInfo = JSON.stringify(userInfo);
 
 		//保存用户信息在本地
 		try{
 			if(window.localStorage) {
-				alert("记住大小和颜色了")
 			    window.localStorage.userInfo =userInfo;
-
-				var userInfo = {};
-				if (window.localStorage.userInfo) {
-		  			var userInfo = JSON.parse(window.localStorage.userInfo);
-					$(".pageCount").css("fontSize",window.userInfo.userSize);
-					$(".pageCount").css("background",window.userInfo.userColor);
-		 	 	}
-				
-				console.log(userInfo)
 			}
 		//catch必须传参
 		}catch(e){}
