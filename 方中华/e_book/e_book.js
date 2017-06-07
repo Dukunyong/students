e7076f1e79ec1bb3c973de0fa5116301c28dc2a8
fangzhonghua <1435340904@qq.com>
2017-06-07 23:12:10 +0800

    fzh

--- 方中华/e_book/e_book.js
+++ 方中华/e_book/e_book.js
@@ -1,7 +1,8 @@
-function Video() {
-	
+function E_book() {
+	this.nowFontsize = $(".js_now_fontsize");
 }
-$.extend(Video.prototype,{
+
+$.extend(E_book.prototype, {
 	init: function(){
 		this.restoreUserInfo();
 		this.bindEvents();
@@ -15,61 +16,67 @@ $.extend(Video.prototype,{
 					var userInfo = JSON.parse(window.localStorage.userInfo);
 				}
 				if(userInfo.bgColor){
-					$("body").css({"backgroundColor": userInfo.bgColor});
+					$("body").attr("class", userInfo.bgColor)
 				}
 				if(userInfo.fontSize){
 					$(".js_content p").css({"font-size": userInfo.fontSize+"px"});
-					$(".js_now_fontsize").text(userInfo.fontSize)
+					this.nowFontsize.text(userInfo.fontSize)
 				}
 			}
 		}catch(e) {};
 	},
 	
 	bindEvents: function() {
-		$(".js_choice_color li").on({"click":this.handleChangeBgClick});
-		$(".js_bigger_fontsize").on({"click":this.handleChangefontsizeClick});
-		$(".js_smaller_fontsize").on({"click":this.handleChangefontsizeClick});
+		$(".js_choice_color li").on("click", this.handleChangeBgClick);
+		$(".js_add_fontsize").on("click", $.proxy(this.handleAddfontsizeClick, this));
+		$(".js_ninus_fontsize").on("click", $.proxy(this.handleMinusfontsizeClick, this));
 	},
 	
 	handleChangeBgClick: function() {
 		var userInfo = {
-			fontSize:$(".js_now_fontsize").text(),
-			bgColor:$(this).css("backgroundColor")
+			fontSize: $(".js_now_fontsize").text(),
+			bgColor: $(this).attr("class")
 		};
-		userInfo=JSON.stringify(userInfo);
+		userInfo = JSON.stringify(userInfo);
 		try{
 			if (window.localStorage) {
-				window.localStorage.userInfo=userInfo;
-			}
+				window.localStorage.userInfo = userInfo;
+			};
 		}catch(e){};
-		$("body").css({"backgroundColor": $(this).css("backgroundColor")});
+		$("body").attr("class", $(this).attr("class"));
 	},
 	
-	handleChangefontsizeClick: function() {
-		if($(this).index()==0){
-			fontSize:$(".js_now_fontsize").text(parseInt($(".js_now_fontsize").text())+2)
-			if(parseInt($(".js_now_fontsize").text())>=50){
-				$(".js_now_fontsize").text(50)
-			}
-		}else{
-			fontSize:$(".js_now_fontsize").text(parseInt($(".js_now_fontsize").text())-2)
-			if(parseInt($(".js_now_fontsize").text())<=8){
-				$(".js_now_fontsize").text(8)
-			}
-		}
-		var userInfo = {
-			bgColor:$("body").css("backgroundColor"),
-			fontSize:$(".js_now_fontsize").text()
+	handleAddfontsizeClick: function() {
+		fontSize: this.nowFontsize.text(parseInt(this.nowFontsize.text()) + 2)
+			if(parseInt(this.nowFontsize.text()) >= 50){
+				this.nowFontsize.text(50);
+			};
+		this.fontSize();
+	},
+	
+	handleMinusfontsizeClick: function() {
+		fontSize: this.nowFontsize.text(parseInt(this.nowFontsize.text()) - 2)
+		if(parseInt(this.nowFontsize.text()) <= 8){
+			this.nowFontsize.text(8);
 		};
-		userInfo=JSON.stringify(userInfo);
+		this.fontSize();
+	},
+	
+	fontSize: function() {
+		var userInfo = {
+			bgColor: $("body").attr("class"),
+			fontSize: this.nowFontsize.text()
+		};
+		userInfo = JSON.stringify(userInfo);
 		try{
 			if (window.localStorage) {
-				window.localStorage.userInfo=userInfo;
-			}
-		}catch(e){};
-		$(".js_content p").css({"font-size": parseInt($(".js_now_fontsize").text())+"px"});
+				window.localStorage.userInfo = userInfo;
+			};
+		}catch(e) {};
+		$(".js_content p").css({"font-size": parseInt(this.nowFontsize.text()) + "px"});
 	}
-})
-var e_book = new Video();
+})	
+
+var e_book = new E_book();
 e_book.init();
 
